bdb1f56180b905dadc67c97e69053d2c40fd781c
Prabhjyot Singh <prabhjyotg@gmail.com>
2020-10-25 17:42:34 -0400

    Updates

---
+++ precache-manifest.4a661a91be1c93a846f63171649954c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e1f3890a2c96a5694fb09d452970e5",
+    "url": "/Youtube-Clone/index.html"
+  },
+  {
+    "revision": "2016cf01851e842c9729",
+    "url": "/Youtube-Clone/static/css/main.301d99c8.chunk.css"
+  },
+  {
+    "revision": "9376828184cecd9b7d4b",
+    "url": "/Youtube-Clone/static/js/2.32f4108e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Youtube-Clone/static/js/2.32f4108e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2016cf01851e842c9729",
+    "url": "/Youtube-Clone/static/js/main.15bc9c2c.chunk.js"
+  },
+  {
+    "revision": "da273a9a8d406110b5d5",
+    "url": "/Youtube-Clone/static/js/runtime-main.b3789ba8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d4782a04abf9294997f15a7ba148a97c.js"
+  "/Youtube-Clone/precache-manifest.4a661a91be1c93a846f63171649954c5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Youtube-Clone/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
